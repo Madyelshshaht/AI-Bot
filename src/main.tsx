5e1b580a5c9e9efc825a81ae561bf26c217c44f8
Mady <madylabtop@gmail.com>
2025-06-29 16:57:03 +0300

    chanegs

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import Store from './store/Store.ts'
-import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={Store}>
